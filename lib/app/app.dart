f715e70a7a9e780995c35a70fba1db7dbd189681
AlexMajumder <alexmajumder55@gmail.com>
2024-12-08 23:30:25 +0600

    fvm

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class CraftyBayApp extends StatefulWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  State<CraftyBayApp> createState() => _CraftyBayAppState();
+}
+
+class _CraftyBayAppState extends State<CraftyBayApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: ,)
+  }
+}
